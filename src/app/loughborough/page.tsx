a616f6288bf01d8cadab3a66b09fc790948b54cb
DESKTOP-A4OL20C\charl <charlotte@burns.net>
2024-12-02 00:07:42 +0000

    test

--- src/app/loughborough/page.tsx
+++ src/app/loughborough/page.tsx
@@ -73,28 +73,28 @@ export default function LoughboroughPage() {
 
           {/* Image Section */}
           <div className="flex-1 flex flex-col gap-4">
-            <div className="relative flex-grow w-full h-full">
+            <div className="rounded relative flex-grow w-full h-full transition-transform duration-300 group-hover:scale-105">
               <Image
                 src="/PetersPizza/pizzapeter.jpg"
                 alt="Peter Pizza"
                 layout="fill"
-                className="rounded object-contain transition-transform duration-300 group-hover:scale-105"
+                className="object-contain"
               />
             </div>
-            <div className="relative flex-grow w-full h-full">
+            <div className="rounded relative flex-grow w-full h-full transition-transform duration-300 group-hover:scale-105">
               <Image
                 src="/PetersPizza/venue1.jpg"
                 alt="Venue"
                 layout="fill"
-                className="rounded object-contain transition-transform duration-300 group-hover:scale-105"
+                className="object-contain"
               />
             </div>
-            <div className="relative flex-grow w-full h-full">
+            <div className="rounded relative flex-grow w-full h-full transition-transform duration-300 group-hover:scale-105">
               <Image
                 src="/PetersPizza/peter-pizzeria-front.jpg"
                 alt="Pizzeria Front"
                 layout="fill"
-                className="rounded object-contain transition-transform duration-300 group-hover:scale-105"
+                className="object-contain"
               />
             </div>
           </div>
