4b6462e2c269f58efbb972bc7611273aead3a93e
DESKTOP-A4OL20C\charl <charlotte@burns.net>
2024-12-03 17:18:49 +0000

    eslint BROTHER PLS

--- src/app/loughborough/page.tsx
+++ src/app/loughborough/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import Navbar from "../components/navbar";
 import SideNav from "../components/sidenav";
